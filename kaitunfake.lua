6b272895f4102306c9ac58c5c382325b5e00d869
DucDesigner18 <nguyenthutrang05112004@gmail.com>
2024-12-22 11:34:11 +0700

    Add files via upload

---
+++ kaitunfake.lua
@@ -0,0 +1,7 @@
+
+-- ditme banana - w-azura - maru - switch - hoho xem anh thể hiện đây này 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DucDesigner18/kaitunvip/refs/heads/main/kaitun-.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DucDesigner18/kaitunvip/refs/heads/main/n%C3%BAt%20copy%20link.ini"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DucDesigner18/kaitunvip/refs/heads/main/fps.lua"))()
