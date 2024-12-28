1ce7e7bc9218d3222eaeb3db7c6fb8d632dc6773
DucDesigner18 <nguyenthutrang05112004@gmail.com>
2024-12-28 21:51:13 +0700

    Update kaitunfake.lua

--- kaitunfake.lua
+++ kaitunfake.lua
@@ -1,7 +1,8 @@
 
 -- ditme banana - w-azura - maru - switch - hoho xem anh thể hiện đây này 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DucDesigner18/kaitunvip/refs/heads/main/fps"))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DucDesigner18/kaitunvip/refs/heads/main/kaitun-.lua"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DucDesigner18/kaitunvip/refs/heads/main/lol"))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DucDesigner18/kaitunvip/refs/heads/main/fps"))()
