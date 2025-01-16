f981866aa3d49785db52f7501f49b7732c4f1c6b
DucDesigner18 <nguyenthutrang05112004@gmail.com>
2025-01-16 16:51:04 +0700

    Update kaitunfake.lua

--- kaitunfake.lua
+++ kaitunfake.lua
@@ -1,3 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Duc18-code/ducsharelinkhub/refs/heads/main/11111"))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DucDesigner18/kaitunvip/refs/heads/main/kaitun2025"))()
